95c545f07e292d8f3804fe7b9722d2b6b59f3096
alejo1306 <mmoyaalejandro@gmail.com>
2023-09-16 11:42:29 -0300

    cumple

---
+++ app.js
@@ -0,0 +1,20 @@
+function empezar() {
+    document.getElementById("parrafo1").style.display = "block";
+}
+
+function siguiente() {
+    document.getElementById("parrafo2").style.display = "block";
+}
+
+
+function siguiente2() {
+    document.getElementById("parrafo3").style.display = "block";
+}
+
+function siguiente3() {
+    document.getElementById("parrafo4").style.display = "block";
+}
+
+function siguiente4() {
+    document.getElementById("parrafo5").style.display = "block";
+}
